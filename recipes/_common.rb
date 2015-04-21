83d6eccc1028d2279f5b33223687466624eb4262
git <dsklopp@gmail.com>
2015-04-21 09:04:02 -0700

    Adding _common recipe to find and set router ips, and deploy consul

--- recipes/_common.rb
+++ recipes/_common.rb
@@ -7,4 +7,22 @@
 # All rights reserved - Do Not Redistribute
 #
 
+begin
+  if defined(node.remotes.routers.ips)
+  end
+rescue NoMethodError
+  routers=search(:node, "recipes:router")
+  ips=[]
+  routers.each do |router|
+    ips << router.ip
+  end
+  node.default.remotes.routers.ips=ips
+end
 
+include_recipe "sensu::default"
+
+# Set some dynamic attributes
+node.default.sensu.redis.host=node.remotes.routers.ips.first
+node.default.sensu.rabbitmq.host=node.remotes.routers.ips.first
+
+include_recipe "guru-sensu::_consul"
